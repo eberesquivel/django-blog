a101b594c8817d92418f4d5b6f167fd44210a400
Eber Esquivel <eberesquivel@gmail.com>
2018-07-11 23:31:25 -0500

    se agrega boton y formulario para agregar post

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.post_list),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detalle, name='post_detalle'),
+    url(r'^post/nuevo/$', views.post_new, name='nuevo_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
-
+from .forms import PostFormulario
+from django.shortcuts import redirect
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(fecha_creacion__lte=timezone.now()).order_by('fecha_creacion')
@@ -11,3 +12,20 @@ def post_list(request):
 def post_detalle(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detalle.html', {'post': post})
+
+def post_new(request):
+        form = PostFormulario()
+        return render(request, 'blog/post_editar.html', {'form': form})
+
+def post_new(request):
+        if request.method == "POST":
+            form = PostFormulario(request.POST)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.autor = request.user
+                post.fecha_publicacion = timezone.now()
+                post.save()
+                return redirect('post_detalle', pk=post.pk)
+        else:
+            form = PostFormulario()
+        return render(request, 'blog/post_editar.html', {'form': form})
