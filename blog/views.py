3cb7f06e58a288484d8669f673d449e1d164a7b8
Eber Esquivel <eberesquivel@gmail.com>
2018-07-18 18:32:41 -0500

    inversion editar post

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
+from django import forms
 from .forms import PostFormulario
 from django.shortcuts import redirect
 # Create your views here.
@@ -28,4 +29,17 @@ def post_new(request):
                 return redirect('post_detalle', pk=post.pk)
         else:
             form = PostFormulario()
+        return render(request, 'blog/post_editar.html', {'form': form})
+
+def post_editar(request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        if request.method == "POST":
+            form = PostFormulario(request.POST, instance=post)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.autor = request.user
+                post.save()
+                return redirect('post_detalle', pk=post.pk)
+        else:
+            form = PostFormulario(instance=post)
         return render(request, 'blog/post_editar.html', {'form': form})
