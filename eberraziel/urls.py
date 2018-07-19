93c8d100e34fb1852a871e52cd616fd650df4ae8
Eber Esquivel <eberesquivel@gmail.com>
2018-07-19 01:41:25 -0500

    instalación jet dashboard admin django

--- eberraziel/urls.py
+++ eberraziel/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.contrib.auth import views
 
 urlpatterns = [
+    url(r'^jet/', include('jet.urls', 'jet')),
+    url(r'^jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/login/$', views.login, name='login'),
     url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
